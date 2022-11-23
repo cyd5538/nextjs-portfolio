470842a93a25bf64d9e6eda192df5f68f47f42c3
cyd5538 <cyd5538@naver.com>
2022-11-23 13:40:30 +0900

    nextjs13 typescript tailwindcss 셋팅

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+const page = () => {
+  return (
+    <div className='bg-[rgb(0,0,51)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+
+    </div>
+  )
+}
+
+export default page
