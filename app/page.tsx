d0eb2b22f8842eeba5f713f15d0f113cffc7e372
cyd5538 <cyd5538@naver.com>
2022-11-23 15:00:56 +0900

    react-social-icon, framer-motion 사용해서 헤더 컴포넌트 완성

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import Header from './Header'
 const page = () => {
   return (
-    <div className='bg-[rgb(0,0,51)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
-
+    <div className='bg-[rgb(20,20,21)] text-white h-screen snap-y snap-mandatory z-0'>
+      <Header />
     </div>
   )
 }
